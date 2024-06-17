066538ed17b7eed760b67fcf932b2cfda8c4ca30
Chenglan Guo <clguo@thoughtworks.com>
2024-06-18 02:16:44 +0800

    remove translate

--- luci/applications/luci-app-nfs/htdocs/luci-static/resources/view/nfs.js
+++ luci/applications/luci-app-nfs/htdocs/luci-static/resources/view/nfs.js
@@ -18,54 +18,54 @@ return view.extend({
     m = new form.Map('nfs', _('NFS Manage'));
 
     // -- NFS Share --
-    s = m.section(form.TypedSection, "share", translate("Shared Directories"));
+    s = m.section(form.TypedSection, "share", _("Shared Directories"));
     s.anonymous = true;
     s.addremove = true;
 
-    o = s.option(form.Flag, "enabled", translate("Enable"));
+    o = s.option(form.Flag, "enabled", _("Enable"));
     o.rmempty = false;
     o.default = "1";
 
-    o = s.option(form.Value, "path", translate("Path"));
+    o = s.option(form.Value, "path", _("Path"));
     o.placeholder = "/mnt";
     o.rmempty = false;
     o.optional = false;
 
-    o = s.option(form.Value, "clients", translate("Clients"));
+    o = s.option(form.Value, "clients", _("Clients"));
     o.placeholder = "192.168.1.0/24";
     o.rmempty = false;
     o.optional = false;
 
-    o = s.option(form.Value, "options", translate("options"));
+    o = s.option(form.Value, "options", _("options"));
     o.placeholder = "rw,sync,root_squash,all_squash,insecure,no_subtree_check";
     o.rmempty = false;
     o.optional = false;
 
     // -- NFS Mount --
-    c = m.section(form.TypedSection, "mount", translate("Mounted Points"));
+    c = m.section(form.TypedSection, "mount", _("Mounted Points"));
     c.anonymous = true;
     c.addremove = true;
 
-    o = c.option(form.Flag, "enabled", translate("Enable"));
+    o = c.option(form.Flag, "enabled", _("Enable"));
     o.default = "1";
     o.rmempty = false;
 
-    o = c.option(form.Value, "source", translate("source"));
+    o = c.option(form.Value, "source", _("source"));
     o.placeholder = "192.168.1.1:/mnt/*";
     o.rmempty = false;
     o.optional = false;
 
-    o = c.option(form.Value, "target", translate("target"));
+    o = c.option(form.Value, "target", _("target"));
     o.placeholder = "/mnt/nfs/*";
     o.rmempty = false;
     o.optional = false;
 
-    o = c.option(form.Value, "options", translate("options"));
+    o = c.option(form.Value, "options", _("options"));
     o.placeholder = "rw,nolock";
     o.rmempty = false;
     o.optional = false;
 
-    o = c.option(form.Value, "delay", translate("delay"));
+    o = c.option(form.Value, "delay", _("delay"));
     o.placeholder = "5";
     o.rmempty = false;
     o.optional = false;
